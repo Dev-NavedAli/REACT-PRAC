20906528185f9f68f18be83899b4eac7e76cce4e
Naved khan <nomanali50997@gmail.com>
2024-12-24 11:40:00 +0530

    prac

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import styled from "styled-components"
+function App() {
+  const DynamicButton = styled.button`
+    color: white;
+    background-color: blue;
+  `;
 
-const App = () => {
-  const [count, setCount] = useState(0);
+  return <DynamicButton>Click Me</DynamicButton>;
+}
 
-  // Log when the count changes
-  useEffect(() => {
-    console.log('Button clicked');
-  }, [count]);
-
-  const buttonStyle = {
-    color: 'red',
-    backgroundColor: 'white',
-    fontWeight: 'bold', // Fixed invalid CSS property
-  };
-
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
-  return (
-    <>
-      <button style={buttonStyle} onClick={handleClick}>
-        Click ME! {count}
-      </button>
-    </>
-  );
-};
 
 export default App;
